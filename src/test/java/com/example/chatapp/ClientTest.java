c6be7097790367e5e9f9b5251db08b0f6c91f9a6
Maes-tr0 <nikita.opara.work@gmail.com>
2024-05-25 00:03:24 +0300

    Make console app: Server, Clint work correct

---
+++ src/test/java/com/example/chatapp/ClientTest.java
@@ -0,0 +1,9 @@
+package com.example.chatapp;
+
+import com.example.chatapp.client.main.Client;
+
+public class ClientTest {
+    public static void main(String[] args) {
+        new Client();
+    }
+}
---
+++ src/test/java/com/example/chatapp/ClientTest2.java
@@ -0,0 +1,9 @@
+package com.example.chatapp;
+
+import com.example.chatapp.client.main.Client;
+
+public class ClientTest2 {
+    public static void main(String[] args) {
+        new Client();
+    }
+}
